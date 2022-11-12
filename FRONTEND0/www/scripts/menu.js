7647e4c63ea2f5cb1ff8ba18ef6081f5ba27a7ec
NunoCartaxoUAL <81565338+NunoCartaxoUAL@users.noreply.github.com>
2022-11-12 16:07:48 +0000

    first commit

---
+++ FRONTEND0/www/scripts/menu.js
@@ -0,0 +1,41 @@
+
+function loadHome(){
+
+    var item_1 = ["name", "images/item_A.png", "drescription" , "price"];
+    var item_2 = ["name2", "images/item_A.png", "drescription2" , "price2"];
+    var item_3 = ["name3", "images/item_A.png", "drescription3" , "price3"];
+    var items=[item_1,item_2,item_3]
+    items.forEach(n => {
+        document.getElementById('content').innerHTML+=`<div class="item"><table>
+            <tr>
+                <td>
+                    <div class="item_image">
+                        <img src="`+n[1]+`">
+                    </div>
+                </td>
+                <td>
+                    <div class="item_info">
+                        `+n[0]+`
+                        `+n[2]+`
+                        `+n[3]+`
+                    </div>
+                    <div class="item_buy">
+                        <button>
+                            BOTAO DE COMPRA
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>`;
+    });
+    
+    
+};
+function loadAbout(){
+    clearPage()
+};
+function clearPage(){
+    
+    document.getElementById('content').innerHTML="";
+};
