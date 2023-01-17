1cdbb02edde7222198dae7d3e1323a9781adfa50
Duarte Cabrita <cabritagamer1@gmail.com>
2023-01-17 15:27:22 +0000

    cookiezona

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -186,3 +186,5 @@ async function makeRequest(url, options) {
         console.log(err);
     }
 }
+
+
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -20,3 +20,29 @@ function loadEdit(){
 function loadSearch(){
     document.getElementById('showSearch')
 }
+
+//  COOKIES
+
+function setCookies(productId, name){
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = productId + "=" + (name || "")  + expires + "; path=/"
+}
+
+function getCookies(productId){
+    var productIDEQ = productId + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(roductIDEQ) == 0) return c.substring(productIDEQ.length,c.length);
+    }
+    return null;
+}
+
+
+
